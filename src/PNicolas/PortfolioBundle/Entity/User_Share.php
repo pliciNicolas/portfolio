a4fe446826b662eb6bef46898d00249acd7e68a3
Pliciweb - Nicolas SOTRON <nicolas.s@pliciweb.com>
2018-05-01 14:29:05 +0200

    Add files ungited

---
+++ src/PNicolas/PortfolioBundle/Entity/User_Share.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Description of User_Share
+ *
+ */
+class User_Share {
+	public $share = null;
+	public $portfolios = [];
+	public $total = null;
+	
+	public function addTransaction(\Transaction $transaction) {
+		$this->addTransactionData($transaction->portfolio, $transaction->share, $transaction->type, $transaction->quantity, $transaction->unit_price, $transaction->fee_fixed, $transaction->fee_percent, $transaction->date);
+	}
+	
+	public function addTransactionData(\Portfolio $portfolio, \Share $share, $type, $quantity, $unit_price, $fee_fixed, $fee_percent, \DateTime $date) {
+		if (is_null($this->total)) {
+			$this->total = new User_Share_Line();
+		}
+		
+		$this->share = $share;
+		
+		if (!array_key_exists($portfolio->id, $this->portfolios)) {
+			$this->portfolios[$portfolio->id] = new User_Share_Line();
+			$this->portfolios[$portfolio->id]->share = $share;
+			$this->portfolios[$portfolio->id]->portfolio = $portfolio;
+		}
+		
+		switch($type) {
+			case Transaction::TYPE_BUY : 
+				
+				// Portfolio
+				$this->portfolios[$portfolio->id]->addBuy($unit_price, $quantity, $fee_fixed, $fee_percent, $date);
+				
+				// Total
+				$this->total->addBuy($unit_price, $quantity, $fee_fixed, $fee_percent, $date);
+
+				break;
+
+			case Transaction::TYPE_SELL : 
+				// Portfolio
+				$this->portfolios[$portfolio->id]->addSell($unit_price, $quantity, $fee_fixed, $fee_percent, $date);
+				
+				// Total
+				$this->total->addSell($unit_price, $quantity, $fee_fixed, $fee_percent, $date);
+
+				break;
+
+			case Transaction::TYPE_DIVIDEND : 
+				$dividend = $unit_price * $quantity;
+				
+				// Portfolio
+				$this->portfolios[$portfolio->id]->addDividend($dividend);
+
+				// Total
+				$this->total->addDividend($dividend);
+
+				break;
+		}
+	}
+	
+	/**
+	 * is Share is active ?
+	 * @return boolean
+	 */	
+	public function isActive() {
+		return 0 < $this->total->quantity;
+		
+	}
+}
---
+++ src/PNicolas/PortfolioBundle/Entity/User_Share_Line.php
@@ -0,0 +1,162 @@
+<?php
+
+/**
+ * Description of User_Share_Line
+ *
+ */
+class User_Share_Line {
+
+    public $share = null;
+    public $portfolio = null;
+
+    // Dividend
+    public $dividend_price = 0; // Amount of dividend
+    public $dividend_count = 0; // Number of dividend
+    public $dividend_average = 0; // Average of dividend
+    protected $dividend_capital = 0; // Usefull to define percent
+    public $dividend_percent = 0; // Percent gain for capital
+    protected $dividend_quantity = 0; // Usefull to define dividend per action per year
+    public $dividend_per_share_per_year = 0; // Dividend by year holding for 1 share
+
+    // Date
+    protected $date_last_first_buying = null; // Last first buying date
+    protected $date_holding = null; // DateInterval
+
+    // Capital Gain
+    public $capitalGain_price = 0;  // Previous capital gain
+    public $capitalGain_count = 0; // Count number of capital gain
+    protected $capitalGain_capital = 0; // Usefull to define capitalGain_percent
+	public $capitalGain_percent = 0; // ??
+
+
+    // Real resume data = Dividend + Capital Gain
+    public $gain_price = 0; // Dividend + Gain Value
+    protected $gain_capital = 0; // 
+    public $gain_percent = 0; // 
+	
+    public $balance = 0; // Sum of all add and minus (Buying + Sell + Dividend)
+
+    // Current capital
+    public $quantity = 0; // Current quantity
+    public $unit_price = 0; // Current unit price
+    public $capital = 0; // $quantity * $unit_price	
+
+    
+
+    /**
+     * Add a dividend operation
+     * @param float $dividend
+     */
+    public function addDividend($dividend) {
+        $this->dividend_price += $dividend;
+        $this->dividend_count += 1;
+        $this->dividend_average = round($this->dividend_price / $this->dividend_count, 2);
+        $this->dividend_capital += $this->capital;
+        $this->dividend_percent = round($this->dividend_price / $this->dividend_capital, 2);
+        $this->dividend_quantity += $this->quantity;
+        $this->dividend_per_share_per_year = round($this->dividend_price / $this->dividend_quantity / $this->getYearHolding(), 2);
+        
+		$this->gain_price = $this->dividend_price + $this->capitalGain_price;
+		$this->gain_capital += $dividend;
+		$this->gain_percent = round($this->gain_price / $this->gain_capital * 100 , 2); 
+        $this->balance += $dividend;
+    }
+	
+	/**
+	 * Number of year holding share
+	 * @return int
+	 */
+	protected function getYearHolding() {
+		if (is_null($this->date_holding)) {
+			$now = new DateTime();
+			$diff = $this->date_last_first_buying->diff($now);
+			return max(1,$diff->format('Y'));
+		}
+		
+		return $this->date_holding->format('Y');
+	}
+	
+	public function getHolding() {
+		$data = $this->date_holding;
+		if (is_null($this->date_holding)) {
+			$now = new DateTime();
+			$data = $this->date_last_first_buying->diff($now);
+		}
+		
+		return $data->format('%Y years %m months %d days');
+	}
+			
+
+    /**
+     * Add sell operation
+     * @param float $unit_price
+     * @param int $quantity
+     * @param float $fee_fixed
+     * @param float $fee_percent
+     */
+    public function addSell($unit_price, $quantity, $fee_fixed, $fee_percent, $date) {
+
+        $fee = $fee_fixed + round($fee_percent * $quantity * $unit_price, 2);
+        $price = ($unit_price * $quantity) - $fee;
+
+        $this->capitalGain_price += ($unit_price - $this->unit_price) * $quantity - $fee;
+        $this->capitalGain_count++;
+        $this->capitalGain_capital += $this->capital;
+        $this->capitalGain_percent = round($this->capitalGain_price / $this->capitalGain_capital * 100, 2);
+        
+		$this->gain_price = $this->dividend_price + $this->capitalGain_price;
+		$this->gain_capital += $price;
+		$this->gain_percent = round($this->gain_price / $this->gain_capital * 100 , 2); 
+        $this->balance += $price;
+        
+        $this->quantity -= $quantity;
+		$this->capital = $this->unit_price * $this->quantity;
+		
+        if (0 == $this->quantity) {
+            $new_diff = $this->date_last_first_buying->diff($date);
+            if (is_null($this->date_holding)) {
+                $this->date_holding = $new_diff;
+            } else {
+                $e = new DateTime('00:00');
+                $f = clone $e;
+                $e->add($this->date_holding);
+                $e->add($new_diff);
+                $this->date_holding = $f->diff($e);
+            }
+            $this->date_last_first_buying = null;
+        }
+    }
+
+    /**
+     * Add buy operation
+     * @param float $unit_price
+     * @param int $quantity
+     * @param float $fee_fixed
+     * @param float $fee_percent
+     * @param DateTime $date
+     */
+    public function addBuy($unit_price, $quantity, $fee_fixed, $fee_percent, $date) {
+        if (is_null($this->date_last_first_buying)) {
+            $this->date_last_first_buying = $date;
+        }
+
+        $fee = $fee_fixed + round($fee_percent * $quantity * $unit_price, 2);
+        $price = ($unit_price * $quantity) + $fee;
+
+        $this->balance -= $price;
+
+		$this->quantity += $quantity;
+		$this->unit_price = round(
+						(
+							$price 
+							+
+							$this->capital 
+						) 
+						/ 
+							$this->quantity
+						,2)
+		;
+		
+		$this->capital = $this->unit_price * $this->quantity;
+    }
+}
